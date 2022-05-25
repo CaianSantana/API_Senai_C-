0efd9e767f743b491e973abc0fc7749a085c30e7
CaianSantana <91359094+CaianSantana@users.noreply.github.com>
2022-05-25 11:04:27 -0300

    primeiro commit

---
+++ chapter/chapter/Repositories/LivroRepository.cs
@@ -0,0 +1,18 @@
+﻿using chapter.Contexts;
+using chapter.Models;
+
+namespace chapter.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly ChapterContext _context;
+        public LivroRepository(ChapterContext context)
+        {
+            _context = context;
+        }
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
