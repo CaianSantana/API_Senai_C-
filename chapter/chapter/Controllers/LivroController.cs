f7109a08de207292edccbaaeef2de00ed899c641
CaianSantana <91359094+CaianSantana@users.noreply.github.com>
2022-05-25 12:18:05 -0300

    Métodos Post, Put, Delete e debug com Insomnia

--- chapter/chapter/Controllers/LivroController.cs
+++ chapter/chapter/Controllers/LivroController.cs
@@ -1,4 +1,5 @@
-﻿using chapter.Repositories;
+﻿using chapter.Models;
+using chapter.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -30,5 +31,78 @@ namespace chapter.Controllers
                 throw new Exception(e.Message);
             }
         }
+        [HttpGet("{id}")]
+        public IActionResult BuscarId(int id)
+        {
+            try
+            {
+                Livro livro = _livroRepository.BuscarId(id);
+
+                if (livro == null)
+                {
+                    return NotFound();
+                }
+                return Ok(livro);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult Cadastrar(Livro livro)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(livro);
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Atualizar(int id, Livro livro)
+        {
+            try
+            {
+                _livroRepository.Atualizar(id, livro);
+                if (id == null)
+                {
+                    return NotFound();
+                }
+                return StatusCode(204);
+            }
+            catch(Exception e)
+            {
+
+                throw new Exception(e.Message);
+
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
     }
 }
